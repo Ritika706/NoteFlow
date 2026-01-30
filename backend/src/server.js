9554531a09581bf8267153bd3b31f9845bb05c30
Ritika Soni <soniritika172006@gmail.com>
2026-01-30 21:57:12 +0530

    Fix Express v5 OPTIONS route

--- backend/src/server.js
+++ backend/src/server.js
@@ -45,7 +45,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 // Public preview support
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
